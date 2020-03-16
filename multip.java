119095a79fbcb4fba0c1fa8a8d1edfc5493b2e72
samaa ahmed <ahmedsky144@gmail.com>
2020-03-16 02:07:07 +0200

    initial commit

---
+++ multip.java
@@ -0,0 +1,23 @@
+package multiplication;
+
+public class multip {
+
+	public static void main(String[] args) {
+		
+		int result =0;
+		for(int i=1;i<=10;i++) {
+			for(int j=1;j<=i;j++) {
+				result=i*j;
+			
+	
+			}
+			System.out.println(result);
+			
+			
+		}
+		
+	 
+
+	}
+
+}
